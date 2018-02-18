87355955677dc35eebe53b538c6c4acff45c577d
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2018-02-18 11:48:15 -0300

    Corrige install vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-if ! command -v code; then
-    return
-fi
-
-
 dotfilespath="$HOME/dotfiles"
 
-rm -rf ~/Library/Application\ Support/Code/User/snippets
-ln -sf "$dotfilespath/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
-ln -sf "$dotfilespath/vscode/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf "$dotfilespath/vscode/snippets/" ~/Library/Application\ Support/Code/User/snippets
+if [ "$(uname)" == "Linux" ]; then
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get update
+    sudo apt-get install code
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    rm -rf ~/Library/Application\ Support/Code/User/snippets
+    ln -sf "$dotfilespath/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
+    ln -sf "$dotfilespath/vscode/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
+    ln -sf "$dotfilespath/vscode/snippets/" ~/Library/Application\ Support/Code/User/snippets
+fi
