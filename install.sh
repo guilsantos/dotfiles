66d4bbc6bc867990d1a4bb685ad4ddcc4d54156b
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2018-06-05 11:03:59 -0300

    Comment some scripts

--- install.sh
+++ install.sh
@@ -3,34 +3,31 @@ set -e
 
 TIMESTAMP=$(date +%s);
 dotfilespath="$HOME/dotfiles"
-dotrepo="https://github.com/rafaellucio/dotfiles.git"
+# dotrepo="https://github.com/rafaellucio/dotfiles.git"
 
-if [ ! -d "$dotfilespath" ]; then
-    printf "Fetching dotfiles...\n"
-    git clone --recursive "$dotrepo" "$dotfilespath"
-fi;
+# if [ ! -d "$dotfilespath" ]; then
+#     printf "Fetching dotfiles...\n"
+#     git clone --recursive "$dotrepo" "$dotfilespath"
+# fi;
 
-sh $dotfilespath/osx/install.sh
-sudo sh $dotfilespath/debian/install.sh
+# sh $dotfilespath/osx/install.sh
+# sudo sh $dotfilespath/debian/install.sh
 
-echo "Installing Oh My Zsh"
-curl -L http://install.ohmyz.sh | bash
-
-echo "Installing NVM"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+# echo "Installing Oh My Zsh"
+# curl -L http://install.ohmyz.sh | bash
 
 
-# Sudo List Files
-VSCODE="$HOME/dotfiles/vscode/install.sh"
+# # Sudo List Files
+# VSCODE="$HOME/dotfiles/vscode/install.sh"
 
-find $dotfilespath -mindepth 2 -name 'install.sh'|grep -v -E "(osx|debian)"| while read FILE; do
-    echo "Running: $FILE"
+# find $dotfilespath -mindepth 2 -name 'install.sh'|grep -v -E "(osx|debian)"| while read FILE; do
+#     echo "Running: $FILE"
     
-    case $FILE in
-        $VSCODE) sudo sh $FILE ;;
-        *) sh $FILE ;;
-    esac
-done
+#     case $FILE in
+#         $VSCODE) sudo sh $FILE ;;
+#         *) sh $FILE ;;
+#     esac
+# done
 
 echo "Configuring .dotfiles"
 find $dotfilespath/* -maxdepth 0 -type f -not -name "install.sh" -not -name "LICENSE" -not -name "README.md" | while read FILE; do
@@ -61,5 +58,4 @@ ln -s "$HOME/dotfiles/ssh" "$HOME/.ssh"
 ln -s "$HOME/dotfiles/ssh/pgpass" "$HOME/.pgpass"
 ln -s "$HOME/dotfiles/ssh/aws" "$HOME/.aws"
 
-chsh -s /bin/zsh
 echo "Finish!!!"
