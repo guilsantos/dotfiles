1121b3dafc2b34925bb2bb7468e293ec6248d02e
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2018-06-05 11:05:20 -0300

    remove unnecessary packages

--- npm/install.sh
+++ npm/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Installing NVM"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
 echo "Nvm running"
 sleep 3
 . $HOME/.nvm/nvm.sh
@@ -8,7 +11,4 @@ nvm install node
 
 npm install -g \
 	gh \
-	pm2 \
-	express-generator \
-	create-react-app \
-	http-server	
+	pm2
