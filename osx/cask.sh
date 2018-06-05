2b81d03f24d4748e481fb7e29dc7cf63adf24029
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2018-06-05 11:25:08 -0300

    remove unnecessary packages

--- osx/cask.sh
+++ osx/cask.sh
@@ -5,20 +5,7 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 brew cask install \
     firefox \
     google-chrome \
-    google-chrome-canary \
     slack \
-    hyper \
     iterm2 \
     spectacle \
-    alfred \
-    visual-studio-code \
-    filezilla \
-
-# drives
-# brew cask install dropbox
-# brew cask install google-drive
-
-# less often
-# brew cask install vlc
-# brew cask install send-to-kindle
-# brew cask install libreoffice
+    visual-studio-code
